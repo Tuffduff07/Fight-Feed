acb0c0ea65498ef74e4a8512c26e60cf2e66b115
Tuffduff07 <duffythomas07@yahoo.com>
2026-05-23 21:26:10 +0100

    Create server.js

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+
+  res.send('<h1>Fight Feed Live 🥊</h1>');
+
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+
+  console.log('Server running');
+
+});
